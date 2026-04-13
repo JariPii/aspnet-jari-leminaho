494c41ae6a8bcb75a2eb89d6a10b15a9336eb5a4
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2026-04-13 18:08:44 +0200

    Adds repositories

---
+++ CoreFitness.Domain/Exceptions/RowVersionException.cs
@@ -0,0 +1,7 @@
+﻿namespace CoreFitness.Domain.Exceptions
+{
+    public class MissingRowVersionException : DomainException
+    {
+        public MissingRowVersionException() : base("Rowversion is required") { }
+    }
+}

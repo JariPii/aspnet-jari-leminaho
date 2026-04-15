6b214d15a35f06b665058d38b9ea266ab9aad024
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2026-04-15 14:39:41 +0200

    Adds basic Repos

--- CoreFitness.Infrastructure/Repositories/TrainingSessionRepository.cs
+++ CoreFitness.Infrastructure/Repositories/TrainingSessionRepository.cs
@@ -1,15 +1,18 @@
 ﻿using CoreFitness.Domain.Entities.TrainingSessions;
 using CoreFitness.Domain.Entities.TrainingSessions.ValueObjects;
 using CoreFitness.Domain.Interfaces.TrainingSessions;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoreFitness.Infrastructure.Repositories
 {
     public class TrainingSessionRepository(CoreFitnessDbContext context) : BaseRepository<TrainingSession, TrainingSessionId>(context),
         ITrainingSessionRepository
     {
-        public Task<IEnumerable<TrainingSession>> GetUpcomingAsync(CancellationToken ct = default)
-        {
-            throw new NotImplementedException();
-        }
+        public async Task<IEnumerable<TrainingSession>> GetUpcomingAsync(CancellationToken ct = default) =>
+            await _context.TrainingSessions
+            .Include(ts => ts.Bookings)
+            .Where(ts => ts.StartDate > DateTimeOffset.UtcNow)
+            .OrderBy(ts => ts.StartDate)
+            .ToListAsync(ct);
     }
 }

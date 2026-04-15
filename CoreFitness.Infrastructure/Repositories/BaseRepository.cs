0cb0d62cebf216f96608767e4ed8e936179cfd50
Jari Leminaho <89419494+JariPii@users.noreply.github.com>
2026-04-15 13:36:00 +0200

    Force push

--- CoreFitness.Infrastructure/Repositories/BaseRepository.cs
+++ CoreFitness.Infrastructure/Repositories/BaseRepository.cs
@@ -45,7 +45,7 @@ namespace CoreFitness.Infrastructure.Repositories
             await _context.Set<T>().FindAsync([id], ct);
 
         public virtual async Task<bool> ExistsByIdAsync(TId id, CancellationToken ct = default) =>
-            await _context.Set<T>().AnyAsync(e => e.Id!.Equals(id), ct);
+            await _context.Set<T>().AnyAsync(e => EqualityComparer<TId>.Default.Equals(e.Id, id), ct);
 
         public virtual async Task<IEnumerable<T>> GetAllAsync(CancellationToken ct = default)
         {
